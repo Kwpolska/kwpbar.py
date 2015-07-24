49ca8eb4459ae72cb34b6af26d5719a763ca1e70
Chris Warrick <kwpolska@gmail.com>
2015-07-24 13:49:24 +0200

    v0.1.0: initial release
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+import sys
+import io
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['tests/']
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+setup(name='kwpbar',
+      version='0.1.0',
+      description='A progress bar for Python.',
+      keywords='kwpbar',
+      author='Chris Warrick',
+      author_email='chris@chriswarrick.com',
+      url='https://github.com/Kwpolska/kwpbar.py',
+      license='3-clause BSD',
+      long_description=io.open('./docs/README.rst', 'r', encoding='utf-8').read(),
+      platforms='any',
+      zip_safe=False,
+      cmdclass={'test': PyTest},
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4'],
+      packages=['kwpbar'],
+      )
